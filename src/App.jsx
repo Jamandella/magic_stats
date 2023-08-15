dc18d7129abdf38bd93cc428420e6f9e284b806c
Brian <brian.elliot.toon@gmail.com>
2023-08-15 16:14:24 -0400

    project objective & useful links

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 function App() {
+  const links = [
+    {name: 'Github Repo', url: 'https://github.com/Jamandella/magic_stats'},
+    {name: 'Figma Wireframe', url: 'https://www.figma.com/file/YqYaigEWARHy5NL5g08Kpq/Magic-Stats-Project?type=whiteboard&t=UKJd6ZK53VYmdl8B-0'},
+    {name: 'Meeting Notes', url: 'https://drive.google.com/drive/folders/126ZhLhKfrLapQaeJPCU4REnxilpgR3qA'},
+    {name: 'Lucid Chart', url: 'https://lucid.app/lucidchart/002d3ba4-b5e1-44b8-9c6c-3b6e62869340/edit?invitationId=inv_d3a938cc-a15e-49bf-8d0a-d7902a952bda&page=0_0#'},
+    {name: '17 Lands', url: 'https://www.17lands.com/'},
+    {name: 'Recharts', url: 'https://recharts.org/en-US/'},
+  ];
+
   return (
     <div className="container">
       <h1 className="accent-text">Magic Stats</h1>
-      <h2>This is a subhead</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci perferendis nemo veritatis nulla rerum suscipit sapiente, laborum laudantium assumenda id non, natus reprehenderit, consequuntur atque magni autem! Recusandae, corrupti cum?
-      Minus quam cumque doloribus accusantium, necessitatibus omnis dicta assumenda earum, excepturi quasi porro laudantium, maiores voluptate quo? Odit, voluptates quam? Maiores a non quo omnis harum corporis sed adipisci aspernatur.
-      Suscipit magni veniam cumque, et at sed debitis, quia accusantium ad officiis consequuntur optio ratione necessitatibus praesentium sequi assumenda explicabo quis asperiores eaque obcaecati recusandae animi provident. Excepturi, voluptatum quasi!</p>
+      <h2>Our Objective</h2>
+      <p>The goal of this project is to provide Magic: the Gathering draft achetype stats. 
+        To accomplish this we will be using data from Magic Arena drafts provided by 17 Lands. 
+      </p>
+
+      <h2>Useful Links</h2>
+      <ul>
+        {links.map(link => (
+          <li key={link.name}>
+            <a className="link" href={link.url} target="_blank">{link.name}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
