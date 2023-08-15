6c23412512d394354df0e731da02304dc950290e
Brian <brian.elliot.toon@gmail.com>
2023-08-15 15:05:11 -0400

    installed sass, created sass folder & deleted starter css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <h1 className="accent-text">Magic Stats</h1>
+      <h2>This is a subhead</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci perferendis nemo veritatis nulla rerum suscipit sapiente, laborum laudantium assumenda id non, natus reprehenderit, consequuntur atque magni autem! Recusandae, corrupti cum?
+      Minus quam cumque doloribus accusantium, necessitatibus omnis dicta assumenda earum, excepturi quasi porro laudantium, maiores voluptate quo? Odit, voluptates quam? Maiores a non quo omnis harum corporis sed adipisci aspernatur.
+      Suscipit magni veniam cumque, et at sed debitis, quia accusantium ad officiis consequuntur optio ratione necessitatibus praesentium sequi assumenda explicabo quis asperiores eaque obcaecati recusandae animi provident. Excepturi, voluptatum quasi!</p>
+    </div>
   )
 }
 
