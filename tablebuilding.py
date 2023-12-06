cc16eb0a29f9ab59da797009162b89bef7990cc8
Ben Bellis <94162169+benbellis@users.noreply.github.com>
2023-12-05 19:25:06 -0500

    Update tablebuilding.py

--- tablebuilding.py
+++ tablebuilding.py
@@ -8,8 +8,8 @@ from dbpgstrings import host, database, user, password
 import time
 
 set_abbr='ltr'#This determines which set we are working with. Current options: ltr, dmu, bro
-#engine1 = create_engine("sqlite:///23spells.db", echo=False) #used to build tables locally
-#conn1 = engine1.connect()
+engine1 = create_engine("sqlite:///23spells.db", echo=False) #used to build tables locally
+conn1 = engine1.connect()
 port='5432'
 engine2=create_engine(url="postgresql://{0}:{1}@{2}:{3}/{4}".format(
             user, password, host, port, database))
@@ -20,26 +20,22 @@ metadata = MetaData()
 class Base(DeclarativeBase):
     pass
 
-"""class What(Base):
-    __tablename__="what"
-    huh=mapped_column(SmallInteger,primary_key=True)
-class TheFuck(Base):
-    __tablename__="theFuck"
-    why=mapped_column(SmallInteger,primary_key=True)
-    que=mapped_column(SmallInteger,ForeignKey('what.huh'))"""
-#the above worked. I don't get what is different.
+
 class ArchRank(Base):
     __tablename__=set_abbr+"ArchRank"
     id=mapped_column(SmallInteger, primary_key=True)
     name=mapped_column(String)
     arc_num=mapped_column(SmallInteger)
     rank=mapped_column(SmallInteger)
-    #id encodes both archetype and rank by id=arc_num*7+rank
-    #currently rank 0=None and rank 1 is Bronze. <1.5% of matches have no recorded rank, and I don't really know how a game ends up unranked.
-    #maybe just scrap unranked? 
+    #lots of things are split up by both archetype and rank, this table encodes both as an int
+    #name: main colors as a string (e.g. "B", "WU", "URG", etc.) (this will change after archetypes get refined)
+    #arcnum: an int corresponding to the archetype
+    #ranks: 0=None, 1=Bronze, 2=Silver, 3=Gold, 4=Platinum, 5=Diamond, 6=Mythic
+    #id encodes both archetype and rank by id=arc_num*7+rank, so id//7=arc_num and id%7=rank
     
 
 class CardInfo(Base):
+    #The information describing each card in the set
     __tablename__=set_abbr+"CardInfo"
     id=mapped_column(SmallInteger, primary_key=True)
     name=mapped_column(String)
@@ -47,18 +43,30 @@ class CardInfo(Base):
     color=mapped_column(SmallInteger)
     card_type=mapped_column(String)
     rarity=mapped_column(String)
-    # color: W=1, U=2, B=4, R=8, G=16, so color=22 means UBG, color=0 means colorless.
+    # name: Card name
+    # color: 5 bit int with each bit representing presence of a color
+    # W=1, U=2, B=4, R=8, G=16, so color:22=10110 means UBG, color=0 means colorless.
     # card type-A(rtifact), C(reature), E(nchantment), L(and), P(laneswalker), I(nstant), S(orcery), B(attle) 
+    # multi-type cards are stored like 'AC' for 'Artifact Creature'.
     # Rarity- C(ommon), U(ncommon), R(are), M(ythic), B(asic land)
 
 class ArchGameStats(Base):
+    #For each archetype and rank and length of game (by turns), there is a row for wins and a row for losses
+    #In these rows the recorded data is the number of games and the total number of cards of each mana value
+    #played in decks in those games.
+    #For example: in the row for arid(UB,silver), turns=8, won=True, 
+    #n2_drops/game_count would correspond to the average number of 2 drops in UB decks at rank silver that won a game in 8 turns.
+    #To get the average number of 2 drops in all UB decks, sum n2_drops and game_count over all rows where the arid corresponds to a UB deck
+    #and divide the totals.
+    #To get the win rate of arid 40 (currently WU diamond) in 6-turn games: total wins=game count from row arid=40, turns=6 won=True
+    #total losses=game_count from row arid=40, turns=6, won=False
     __tablename__=set_abbr+"ArchGameStats"
-    arid=mapped_column(SmallInteger, ForeignKey(set_abbr+'ArchRank.id',ondelete='CASCADE',onupdate='CASCADE'),primary_key=True)
+    arid=mapped_column(SmallInteger, ForeignKey(set_abbr+'ArchRank.id',ondelete='CASCADE',onupdate='CASCADE'),primary_key=True) 
     won=mapped_column(Boolean, primary_key=True)
     turns=mapped_column(SmallInteger, primary_key=True) #how long the game lasted
     game_count=mapped_column(Integer) #number of games that fit the preceding criteria
     lands=mapped_column(Integer) #total number of lands in decks that played those games
-    n0_drops=mapped_column(Integer) #total number of 0 mana spells
+    n0_drops=mapped_column(Integer) #total number of 0 mana cards in all of those games
     n1_drops=mapped_column(Integer)
     n2_drops=mapped_column(Integer) 
     n3_drops=mapped_column(Integer)
@@ -67,9 +75,23 @@ class ArchGameStats(Base):
     n6_drops=mapped_column(Integer)
     n7_drops=mapped_column(Integer)
     n8p_drops=mapped_column(Integer)
+    #arid: int that encodes both archetype and rank via the ArchRank table
+    #won: whether this row is wins or losses
+    #turns: how many turns the game lasted
+    #game_count: number of games that match the previous 3 variables (e.g. number of times a WR diamond deck lost in 9 turns)
+    #lands: total number of lands in deck in those games
+    #nx_drops: total number of x drops in deck in those games (with n8p_drops meaning number of 8+ drops)
+    
 
 class CardGameStats(Base):
     #better structure id/arid/copies : wins/losses
+    #id: card id from CardInfo table
+    #arid: archetype and rank encoded as one int from ArchRank table
+    #copies: number of copies in deck
+    #win count: number of wins in deck
+    #game count: number of games in deck
+    #game played win rate=sum over copies>=1 of win_count/sum over copies>=1 of game_count
+    #more stats to come
     __tablename__=set_abbr+"CardGameStats"
     id=mapped_column(SmallInteger, ForeignKey(set_abbr+'CardInfo.id',ondelete='CASCADE',onupdate='CASCADE'), primary_key=True)
     arid=mapped_column(SmallInteger, ForeignKey(set_abbr+'ArchRank.id',ondelete='CASCADE',onupdate='CASCADE'), primary_key=True)
@@ -185,9 +207,10 @@ def populateCardGameTable(conn): #this takes a while (~20 mins on 1M game LTR da
         conn.commit()
     conn.commit()
 
-def populateCardGameTableV2(conn): #maybe slightly faster. It's small enough to be uncertain.
-    #next idea to speed up: process game_data in chunks e.g. take 50K(?) consecutive lines at a time. 
-    #if that works, also should apply to archgametable
+def populateCardGameTableV2(conn): #slightly faster. Small enough difference to be uncertain.
+    #is there a good way to process all cards simultaneously so that each game only gets looked at once?
+    #might read faster, but jumping around the write df could be costly.
+    #maybe check pandas methods for something helpful
     cg_table_name=set_abbr+'CardGameStats'
     cg_table=Base.metadata.tables[cg_table_name]
     d=delete(cg_table)
@@ -227,6 +250,46 @@ def populateCardGameTableV2(conn): #maybe slightly faster. It's small enough to 
         conn.commit()
     print("Total time:",time.time()-t0)
     conn.commit()
+
+def populateCardGameTableV3(conn): #Was worth a shot, but ended up slower. 
+    #Possible time save: Query multiple columns at a time (can't take too many for memory reasons I think)
+    #As is, took 10 cards in 1 minute->27 minutes for whole set (about 35% slower than others)
+    cg_table_name=set_abbr+'CardGameStats'
+    cg_table=Base.metadata.tables[cg_table_name]
+    arID_table=Base.metadata.tables[set_abbr+'ArchRank']
+    metadata.reflect(bind=conn1)
+    game_data_table=metadata.tables[set_abbr+'GameData']
+    ranks=[None,'bronze','silver','gold','platinum','diamond','mythic']
+    q1=select(arID_table)
+    aridDF=pd.read_sql_query(q1,conn,index_col='id')
+    cardDF=cardInfo(set_abbr)
+    t0=time.time()
+    #for n in cardDF.index:
+    for n in range(10):
+        t1=time.time()
+        card=cardDF.at[n,'name']
+        card_id=n
+        col='deck_'+card 
+        q=select(game_data_table.c.main_colors,game_data_table.c.rank,getattr(game_data_table.c,col),game_data_table.c.won)
+        df=pd.read_sql_query(q,conn)
+        print("Query took", time.time()-t1) 
+        print("Counting copies of {}".format(card))
+        max_copies=int(df[col].max())
+        insertdf=pd.DataFrame({'id':[],'arid':[],'copies':[],'win_count':[], 'game_count':[]})
+        valdf=df.value_counts()
+        for arid in aridDF.index:
+            colors=aridDF.loc[arid,'name']
+            rank=aridDF.loc[arid,'rank']
+            rankName=ranks[rank]
+            for copies in range(max_copies+1):
+                row=insertdf.shape[0]
+                insertdf.loc[row]=[card_id,arid,copies,0,0]
+                if (colors,rankName,copies,1) in valdf.index:insertdf.loc[row,['win_count','game_count']]+=[valdf[colors,rankName,copies,1],valdf[colors,rankName,copies,1]]
+                if (colors,rankName,copies,0) in valdf.index:insertdf.loc[row,['win_count','game_count']]+=[0,valdf[colors,rankName,copies,0]]
+
+        print("Card {} took {}".format(n,time.time()-t1))
+        print(insertdf.iloc[10:20,:])
+    print("Total time",time.time()-t0)
 def populateCardStats(): #todo: make the actual table, fill with all stats that would be indexed by arc/card/rankrange
     #incomplete
     arID_table=Base.metadata.tables[set_abbr+'ArchRank']
@@ -287,6 +350,7 @@ def populateImpacts():
     return df #temporarily outputs a dataframe rather than building a table
 
 def buildDB(conn): #Only run this if you are building/rebuilding from the ground up 
+    #calling with conn1 builds locally, conn2 builds online
     Base.metadata.drop_all(bind=conn)
     Base.metadata.create_all(bind=conn)
     conn.commit()
@@ -301,6 +365,6 @@ def buildDB(conn): #Only run this if you are building/rebuilding from the ground
     print("Done")
     conn.commit()
 
-populateCardGameTable(conn2)
 
+conn1.close()
 conn2.close()
